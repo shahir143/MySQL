cf8284d154f1e13b992cee7797508dcc3bec3ca7
shahir143 <Shahirsyed143@gmail.com>
2023-11-21 15:04:00 +0530

    Making SQL easy with Sequelize

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db=require('./util/database')
+const sequelize=require('./util/database')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,4 +21,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(4000);
+sequelize.sync().then(result=>{
+    console.log(result);
+    app.listen(4000);
+}).catch(err=>console.log(err));
--- models/product.js
+++ models/product.js
@@ -1,32 +1,26 @@
-const db=require('../util/database')
+const Sequelize= require('sequelize');
+const sequelize= require('../util/database');
 
-const getProductsFromFile = cb => {
- 
-};
+const Product= sequelize.define('product',{
+  id:{
+    type:Sequelize.INTEGER,
+    autoIncrement:true,
+    allowNull:false,
+    primaryKey:true
+  },
+  title:Sequelize.STRING,
+  price:{
+    type:Sequelize.DOUBLE,
+    allowNull:false,
+  },
+  imageUrl:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  desciption:{
+    type:Sequelize.STRING,
+    allowNull:false
+  }
+})
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    getProductsFromFile(products => {
-      return db.execute('INSERT INTO products(title,price,description,imageUrl) VALUES (?,?,?,?)', [this.title,this.price,this.imageUrl,this.description])
-  })
-  }
-  static deleteById(id) {
-    return db.execute('DELETE FROM products where products.id=?',[id])
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-   return db.execute('SELECT * FROM products WHERE products.id=?',[id]);
-  }
-};
+module.exports=Product;
