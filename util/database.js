8c2e43c5aec4fd298f675eb892b3d11c0c9c4997
shahir143 <Shahirsyed143@gmail.com>
2023-11-22 13:31:30 +0530

    final changes

--- util/database.js
+++ util/database.js
@@ -1,6 +1,8 @@
-const Sequelize= require('sequelize');
-const sequelize=new Sequelize('node-connected','root','root',{
-    dialect:'mysql',
-    host:'localhost'
-})
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-connected', 'root', 'root', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
 module.exports = sequelize;
