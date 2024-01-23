746ee631556c620d31b0e2a566832148004c183b
shahir143 <Shahirsyed143@gmail.com>
2024-01-23 12:11:07 +0530

    NO SQL complete showdown

--- app.js
+++ app.js
@@ -34,14 +34,14 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://root:root@cluster0.lfqxftn.mongodb.net/?retryWrites=true&w=majority'
   )
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: 'Shahir',
+          email: 'Shahir@test.com',
           cart: {
             items: []
           }
